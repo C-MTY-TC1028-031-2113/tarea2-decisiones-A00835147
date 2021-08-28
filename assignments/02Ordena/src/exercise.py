9d8eeed3e3a1db68f77fcda39fc61c3b59b8c45f
Josue-Isaac <A00835147@tec.mx>
2021-08-28 17:43:49 -0500

    tarea2

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -1,8 +1,7 @@
 def main():
     a=int(input("Ingresa el primer número: "))
     b=int(input("Ingresa el segundo número: "))
-    c=int(input("Ingresa el tercer número: "))
-    
+    c=int(input("Ingresa el tercer número: "))  
     if a>b:
         if b<c:
             if a>c:
