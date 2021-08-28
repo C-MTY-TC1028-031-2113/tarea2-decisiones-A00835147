f2cbe3fb52f68259966e5eb2b804f6fa59bad856
Josue-Isaac <A00835147@tec.mx>
2021-08-28 17:35:32 -0500

    tarea2

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,7 +1,15 @@
-
 def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+    edad=int(input("Dime tu edad: "))
+    
+    if edad>=18:
+        iD=str(input("¿Tienes identificación oficial? (s/n): "))
+        if iD=="s":
+            print("Trámite de licencia concedido")
+        elif iD=="n":
+            print("No cumples requisitos")
+        else:
+            print("Respuesta incorrecta") 
+    else:
+        print("No cumples requisitos")
+        
+main()
